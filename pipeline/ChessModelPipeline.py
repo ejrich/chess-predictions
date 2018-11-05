c2e83c19de97753220c2789130283ada0ce6f067
Evan Richardson <ejrich44@gmail.com>
2018-11-04 20:33:58 -0600

    Updating the notebook and prepping for random forest regressor work

--- pipeline/ChessModelPipeline.py
+++ pipeline/ChessModelPipeline.py
@@ -1,6 +1,7 @@
 import sys, os
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.externals import joblib
 from sklearn.linear_model import SGDRegressor, SGDClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, log_loss, mean_squared_error
@@ -23,6 +24,7 @@ def createLinearRegression(dataFrame, all_columns, dummy_columns):
     X_train, X_test = create_feature_matrices(df_train, df_test, all_columns, dummy_columns)
 
     all_lr = SGDRegressor(max_iter=2000)
+    # all_lr = RandomForestRegressor(max_iter=2000)
     all_lr.fit(X_train, y_train)
     pred_result_all_test = all_lr.predict(X_test)
     all_mse = mean_squared_error(y_test, pred_result_all_test)
